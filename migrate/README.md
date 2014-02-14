e9340a9f4f83cb921a49b15e89ebd24fc32c6809
lxbarth <alex@mapbox.com>
2014-02-14 06:44:43 -0500

    Migrate to Github from CSV step one, needs more work. See #1

---
+++ migrate/README.md
@@ -0,0 +1,12 @@
+# Migrate existing data to Github
+
+We can remove this once migrated.
+
+## Usage
+
+    npm install
+    npm start
+
+## Requirements
+
+    node 0.10.x
---
+++ migrate/index.js
@@ -0,0 +1,46 @@
+var csv = require('csv');
+var fs = require('fs');
+var path = require('path');
+var _ = require('underscore');
+var yaml = require('js-yaml');
+
+var header = [];
+var states = {};
+var keys = [
+    'state',
+    'city',
+    'county',
+    'license',
+    'year',
+    'availability'
+];
+csv().from.stream(fs.createReadStream(process.argv[2]))
+    .on('record', function(row, index){
+        if (index == 0) { header = row; return; }
+        row = _(row).reduce(function(memo, col, i) {
+            if (keys.indexOf(header[i]) === -1) return memo;
+            memo[header[i]] = col;
+            return memo;
+        }, {});
+        // Toss all that don't have a URL
+        if (row['availability'].trim() != '') {
+            if (row['state'].trim() == '') {
+                row['state'] = 'General';
+            }
+            var state = row['state'];
+            delete row['state'];
+            states[state] = states[state] || [];
+            states[state].push(row);
+        }
+    })
+    .on('end', function(count){
+        var dir = '../' + process.argv[2].split('.')[0];
+        try { fs.mkdirSync(dir); } catch(Error) {}
+        _(states).each(function(state, key) {
+            fs.writeFile(path.join(dir, key + '.yml'), yaml.safeDump(state));
+            console.log('Exported ' + path.join(dir, key + '.yml'));
+        });
+    })
+    .on('error', function(error){
+        console.error(error.message);
+    });
