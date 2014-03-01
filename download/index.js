f5cd414f7bb878123252993db508ff78335c18ea
lxbarth <alex@mapbox.com>
2014-03-01 12:49:16 -0500

    Make sure program exits even if sockets hang.

--- download/index.js
+++ download/index.js
@@ -6,11 +6,13 @@ var _ = require('underscore');
 var yaml = require('js-yaml');
 var colors = require('colors');
 var Ftp = require('ftp');
+var Step = require('step');
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 var argv = require('minimist')(process.argv.slice(2));
 
-var downloadHTTP = function(address, test) {
+var downloadHTTP = function(address, test, callback) {
+    callback = callback || function() {};
     var options = {
         url: address.data,
         timeout: 7000
@@ -18,15 +20,17 @@ var downloadHTTP = function(address, test) {
     (test ? request.head : request.get)(options, function(err, res) {
         if (err) {
             console.log(("ERROR\t" + err + " " + address.data).red);
-            return;
+            return callback();
         }
         res.statusCode == 200 ?
             console.log(res.statusCode + " OK\t" + address.data) :
             console.log((res.statusCode + " ERROR\t" + address.data).red);
+        callback();
     }).setMaxListeners(20);
 };
 
-var downloadFTP = function(address, test) {
+var downloadFTP = function(address, test, callback) {
+    callback = callback || function() {};
     var ftp = new Ftp();
     var opt = url.parse(address.data);
     opt.user = (opt.auth || ':').split(':')[0];
@@ -37,49 +41,69 @@ var downloadFTP = function(address, test) {
             if (err) {
                 console.log(("FTP\t" + err + " " + address.data).red);
                 ftp.destroy();
-                return;
+                return callback();
             }
             console.log("FTP OK\t" + address.data);
             if (test) {
+                stream.unref();
                 stream.destroy();
                 ftp.destroy();
             }
+            callback();
         });
     });
     ftp.on('error', function(err) {
         console.log(("FTP\t" + err + " " + address.data).red);
         ftp.destroy();
+        callback();
     });
     ftp.connect(opt);
 };
 
-var download = function(address, test) {
+var download = function(address, test, callback) {
+    callback = callback || function() {};
     if (!address.data) {
         console.log(("NOTICE: No data URL for " + address.website).yellow);
-        return;
+        return callback();
     }
     var options = url.parse(address.data);
     options.protocol == 'ftp:' ?
-        downloadFTP(address, test) :
-        downloadHTTP(address, test);
+        downloadFTP(address, test, callback) :
+        downloadHTTP(address, test, callback);
 };
 
-var downloadFile = function(file, test) {
+var downloadFile = function(file, test, callback) {
+    callback = callback || function() {};
     fs.readFile(file, 'utf8', function(err, data) {
         if (err) { 
-            return;
+            return callback();
         }
-        var addresses = yaml.safeLoad(data);
-        _(addresses).each(function(address) {
-            download(address, test);
-        });
+        Step(
+            function() {
+                var parallel = this.parallel;
+                var addresses = yaml.safeLoad(data);
+                _(addresses).each(function(address) {
+                    download(address, test, parallel());
+                });
+            },
+            callback
+        );
     });
 };
 
 _(['us', 'canada']).each(function(dir) {
     fs.readdir('../us', function(err, files) {
-        _(files).each(function(file) {
-            downloadFile('../us/' + file, argv['test']);
-        });
+        var steps = [];
+        Step(
+            function() {
+                var parallel = this.parallel;
+                _(files).each(function(file) {
+                    downloadFile('../us/' + file, argv['test'], parallel());
+                });
+            },
+            function() {
+                process.exit(0);
+            }
+        );
     });
 });
