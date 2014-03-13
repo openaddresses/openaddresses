46c1a00509739a70ad69bac2ee259987da854406
Tom MacWright <tom@macwright.org>
2014-03-13 13:01:07 -0700

    Add travis, harcode types, add tests, remove code

---
+++ test/sources.js
@@ -0,0 +1,27 @@
+var test = require('tape').test,
+    glob = require('glob'),
+    fs = require('fs'),
+    connectors = require('openaddresses-download').connectors;
+
+test('sources', function(t) {
+    var sources = glob.sync('sources/*.json');
+    t.plan(sources.length);
+    sources.forEach(function(source) {
+        t.test(source, function(t) {
+            t.doesNotThrow(function() {
+                var data = JSON.parse(fs.readFileSync(source, 'utf8'));
+                if (data.type) {
+                    t.ok(connectors[data.type], 'type is valid');
+                    connectors[data.type](data, function(err, resp) {
+                        t.notOk(err, 'response is good');
+                        if (resp) resp.end();
+                        t.end();
+                    });
+                } else {
+                    t.ok('has no type or data');
+                    t.end();
+                }
+            }, source + ' is valid json');
+        });
+    });
+});
