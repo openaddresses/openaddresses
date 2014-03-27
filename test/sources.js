2debe09e4c1bd4bcf09ceec9aa32bcec0728e188
ingalls <nicholas.ingalls@gmail.com>
2014-03-27 00:09:31 -0300

    Fixing Travis Tests

--- test/sources.js
+++ test/sources.js
@@ -12,32 +12,39 @@ glob.sync('sources/*.json').forEach(function(source) {
             t.doesNotThrow(function() {
                 var data = JSON.parse(fs.readFileSync(source, 'utf8'));
                 if (data.type) {
-                    t.ok(connectors[data.type], 'type is valid');
-                    if (data.skip) {
-                        t.ok(source + 'skipping');
+                    if (data.type = "ESRI") {
+                        t.ok('Ignoring ESRI type - skipping');
                         t.end();
                         callback();
-                    } else {
-                        connectors[data.type](data, function(err, resp) {
-                            t.ok(resp, 'response is good');
-                            t.notOk(err, 'no error returned');
-                            if (!resp) {
-                                callback();
-                                return t.end();
-                            }
-                            resp.once('error', function(e) {
-                                t.fail('server failed: ' + e.message);
-                                resp.end();
-                                t.end();
-                                callback();
+                    } else
+                        t.ok(connectors[data.type], 'type is valid');
+                        
+                        if (data.skip) {
+                            t.ok(source + 'skipping');
+                            t.end();
+                            callback();
+                        } else {
+                            connectors[data.type](data, function(err, resp) {
+                                t.ok(resp, 'response is good');
+                                t.notOk(err, 'no error returned');
+                                if (!resp) {
+                                    callback();
+                                    return t.end();
+                                }
+                                resp.once('error', function(e) {
+                                    t.fail('server failed: ' + e.message);
+                                    resp.end();
+                                    t.end();
+                                    callback();
+                                });
+                                resp.once('data', function(d) {
+                                    t.pass('received data');
+                                    t.end();
+                                    resp.end();
+                                    callback();
+                                });
                             });
-                            resp.once('data', function(d) {
-                                t.pass('received data');
-                                t.end();
-                                resp.end();
-                                callback();
-                            });
-                        });
+                        }
                     }
                 } else {
                     t.ok('has no type or data');
