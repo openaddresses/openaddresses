c3d4d87c0f5b975368804f28bb4acd82e4bff77b
ingalls <nick@mapbox.com>
2014-05-04 18:42:57 -0400

    Process Stats

--- stats.js
+++ stats.js
@@ -13,3 +13,47 @@ for (var i = 0; i < sources.length; i++) {
         i--;
     }
 }
+
+var total = sources.length;
+var processed = [],
+    unprocessed = [],
+    cached = [],
+    uncached = [],
+    skip = [];
+
+
+
+sources.forEach(function(source){
+    fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+            return new Error("Can't Access: '" + source + "'");
+        }
+        
+        try {
+            var data = JSON.parse(data);
+        } catch (err) {
+            return new Error("Invalid JSON: '" + source + "'");    
+        }
+        
+        if (data.processed)
+            processed.push(source);
+        else
+            unprocessed.push(source);
+        
+        if (data.cached)
+            cached.push(source);
+        else
+            uncached.push(source);
+        
+        if (data.skip)
+            skip.push(source);
+    });
+});
+
+console.log("OpenAddresses Stats Report\n");
+console.log("Total Sources: " + total);
+console.log("-------------------");
+console.log("Cached: " + cached.length);
+console.log("Uncached: " + total - cached.length);
+console.log("Processed: " + processed.length);
+console.log("Unprocessed: " + total - processed.length);
