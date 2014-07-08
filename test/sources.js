4bb9ae987693e72d39693fe375dec557a60b0e29
ingalls <nick@mapbox.com>
2014-07-07 21:31:13 -0400

    Require a few simple fields

--- test/sources.js
+++ test/sources.js
@@ -22,10 +22,18 @@ function checkSource(i){
                 console.log('[WARN] Skip flag Detected!');
             }
 
+            t.ok(data.data, "Data URL Included");
+
+            t.ok(data.type, "Data URL Type Included");
+
+            t.ok(data.coverage, "Data Coverage Included");
+
             t.pass('Incorrect Type');
             t.end();
             checkSource(++index);
         }, source + ' is valid json');
     });
+
+
 
 }
