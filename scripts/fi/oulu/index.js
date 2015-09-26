963be8566a0307606e51828f1a35668651c4bfe6
Hannes Junnila <hannes.junnila@gmail.com>
2015-09-26 19:44:15 +0300

    Add Oulu, Finland

---
+++ scripts/fi/oulu/index.js
@@ -0,0 +1,35 @@
+var fs        = require('fs')
+  , http      = require('http')
+  , XmlStream = require('xml-stream')
+  ;
+
+
+var out = fs.createWriteStream('osoitteet.txt', {'flags': 'w'});
+out.write("STREET,NUMBER,LAT,LON\n")
+
+var addresses = [];
+var streets = {};
+
+http.get('http://kartta.ouka.fi/web/siirto/osoitteet.xml').on('response', function(res) {
+  res.setEncoding('utf8');
+  var xml = new XmlStream(res); 
+  
+  xml.on('endElement: Address', function(item) {
+    addresses.push({
+      number: item.AddressNumber,
+      streetId: item.IdStreetName,
+      x: item.xVis,
+      y: item.yVis,
+    });
+  });
+  
+  xml.on('endElement: StreetName', function(item) {
+    streets[item.IdStreetName] = item.NameLang1
+  });
+
+  xml.on('endElement: teklaXcitySearchConfiguration', function(){
+    addresses.forEach(function(address){
+      out.write(streets[address.streetId] + ',' + address.number + ',' + address.x + ',' + address.y + '\n');
+    });
+  });
+});
