4a3d2f59119fa22ec2232c89fc4a1e0585b738fe
Michal Migurski <mike-github@teczno.com>
2016-06-17 04:54:17 +0000

    Updated to actual parcels layer

--- scripts/us/tn/cache.sh
+++ scripts/us/tn/cache.sh
@@ -7,8 +7,8 @@ mkdir /work/shp
 TMP=/work openaddr-esri2geojson \
     -p "token=${TOKEN}" -H 'Host: tnmap.tn.gov' \
     -H 'Referer: http://tnmap.tn.gov/assessment/' \
-    http://tnmap.tn.gov/arcgis/rest/services/ADMINISTRATIVE_BOUNDARIES/LEGISLATIVE_DISTRICTS/MapServer/2 \
-    /work/shp/us-tn-districts.shp
+    http://tnmap.tn.gov/arcgis/rest/services/CADASTRAL/STATEWIDE_PARCELS/MapServer/0 \
+    /work/shp/us-tn-parcels.shp
 
 mkdir -p /work/cache/us
-zip -j /work/cache/us/tn.zip /work/shp/us-tn-districts.*
+zip -j /work/cache/us/tn.zip /work/shp/us-tn-parcels.*
