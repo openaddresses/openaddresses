df6dea02e95b2fdff1b423ebf706423499369a0a
ingalls <ingalls@protonmail.com>
2018-03-10 21:51:24 -0500

    Add V2 Schema

--- test/sources_validator.js
+++ test/sources_validator.js
@@ -4,20 +4,29 @@ const fs = require('fs');
 const Ajv = require('ajv');
 const request = require('request');
 const schema = require('../schema/source_schema.json');
+const schema_v2 = require('../schema/source_schema_v2.json');
 
 const ajv = new Ajv();
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'), "http://json-schema.org/draft-04/schema#");
 ajv.addMetaSchema(require('./geojson.json'), "http://json.schemastore.org/geojson#/definitions/geometry");
-testAllSources(ajv.compile(schema));
 
-function testAllSources(validate) {
+const validate = ajv.compile(schema);
+const validate_v2 = ajv.compile(schema_v2);
+
+function testAllSources() {
     // find all the sources, has to be synchronous for tape
     glob.sync('sources/**/*.json').forEach((source) => {
         tape(`tests for ${source}`, (test) => {
             test.test(`schema-validation for source ${source}`, (t) => {
                 try {
                     const data = JSON.parse(fs.readFileSync(source, 'utf8'));
-                    const valid = validate(data);
+
+                    let valid = true;
+                    if (data.schema && data.schema === '2') {
+                        valid = validate_v2(data);
+                    } else {
+                        valid = validate(data);
+                    }
 
                     t.ok(valid, `${source}: ${JSON.stringify(validate.errors)}`);
 
