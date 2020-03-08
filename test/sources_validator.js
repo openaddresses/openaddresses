6b368684748ee37657687a7d1e02e8719e1e1fa8
ingalls <nick@ingalls.ca>
2020-03-08 15:12:10 -0700

    Update to V2 Schema

--- test/sources_validator.js
+++ test/sources_validator.js
@@ -4,33 +4,38 @@ const fs = require('fs');
 const Ajv = require('ajv');
 const request = require('request');
 const schema = require('../schema/source_schema.json');
+const schema_v2 = require('../schema/source_schema_v2.json');
 
-const ajv = new Ajv();
+const ajv = new Ajv({
+    schemaId: 'auto'
+});
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'), "http://json-schema.org/draft-04/schema#");
 ajv.addMetaSchema(require('./geojson.json'), "http://json.schemastore.org/geojson#/definitions/geometry");
-testAllSources(ajv.compile(schema));
 
-function testAllSources(validate) {
-    // find all the sources, has to be synchronous for tape
-    glob.sync('sources/**/*.json').forEach((source) => {
-        tape(`tests for ${source}`, (test) => {
-            test.test(`schema-validation for source ${source}`, (t) => {
-                try {
-                    const data = JSON.parse(fs.readFileSync(source, 'utf8'));
-                    const valid = validate(data);
+const validate = ajv.compile(schema);
+const validate_v2 = ajv.compile(schema_v2);
 
-                    t.ok(valid, `${source}: ${JSON.stringify(validate.errors)}`);
+// find all the sources, has to be synchronous for tape
+glob.sync('sources/**/*.json').forEach((source) => {
+    tape(`tests for ${source}`, (t) => {
+        try {
+            const data = JSON.parse(fs.readFileSync(source, 'utf8'));
 
-                } catch (err) {
-                    t.fail(`could not parse ${source} as JSON: ${err}`);
-                }
+            let valid = true;
+            if (data.schema && data.schema === 2) {
+                valid = validate_v2(data);
 
-                t.end();
+                t.ok(valid, `${source}: ${JSON.stringify(validate_v2.errors)}`);
+            } else {
+                valid = validate(data);
 
-            });
+                t.ok(valid, `${source}: ${JSON.stringify(validate.errors)}`);
+            }
 
-        });
+        } catch (err) {
+            t.fail(`could not parse ${source} as JSON: ${err}`);
+        }
 
+        t.end();
     });
-
-}
+});
