b42e6922e67a2ebd1bc6eb70a2c6ed3f2e1709ad
ingalls <nick@ingalls.ca>
2020-10-20 10:44:51 -0400

    parse

--- test/lib.js
+++ test/lib.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const SCHEMA_V1 = JSON.stringify(fs.readFileSync(path.resolve(__dirname, '../schema/source_schema.json')));
-const SCHEMA_V2 = JSON.stringify(fs.readFileSync(path.resolve(__dirname, '../schema/source_schema_v2.json')));
+const SCHEMA_V1 = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../schema/source_schema.json')));
+const SCHEMA_V2 = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../schema/source_schema_v2.json')));
 
 module.exports = {
     schema: {
