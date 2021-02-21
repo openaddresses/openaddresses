8013809ee801f12a31027f4b202935927c0bc4b0
Vitor George <vitor.george@gmail.com>
2021-02-21 15:11:56 +0000

    Several script updates

--- scripts/br/download-sources.sh
+++ scripts/br/download-sources.sh
@@ -2,4 +2,5 @@
 
 mkdir -p downloads
 wget -N -nv -P downloads/addresses/ -r ftp://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Cadastro_Nacional_de_Enderecos_Fins_Estatisticos/
-wget -N -nv -P downloads/faces/ -r ftp://geoftp.ibge.gov.br/recortes_para_fins_estatisticos/malha_de_setores_censitarios/censo_2010/base_de_faces_de_logradouros_versao_2019/
+wget -N -nv -P downloads/faces/2010 -r ftp://geoftp.ibge.gov.br/recortes_para_fins_estatisticos/malha_de_setores_censitarios/censo_2010/base_de_faces_de_logradouros_versao_2010/
+wget -N -nv -P downloads/faces/2010 -r ftp://geoftp.ibge.gov.br/recortes_para_fins_estatisticos/malha_de_setores_censitarios/censo_2010/base_de_faces_de_logradouros_versao_2019/
--- scripts/br/parse_cnefe.py
+++ scripts/br/parse_cnefe.py
@@ -8,7 +8,8 @@ import csv
 import sys
 from shapely.geometry import mapping, shape
 
-FACES_SOURCE = '/downloads/faces'
+FACES_2019_SOURCE = '/downloads/faces/2019'
+FACES_2010_SOURCE = '/downloads/faces/2010'
 ADDRESSES_SOURCE = '/downloads/addresses'
 SHP_PATH = '/tmp/shp'
 ADDRESSES_PATH = '/tmp/addresses'
@@ -49,148 +50,256 @@ txt_schema = [
 csv_headers = [a for (a, b, c) in txt_schema]
 csv_headers.extend(['subdistrict', 'district', 'municipality', 'state'])
 
+
+def log_error(txt):
+    with open(RESULTS_PATH + "/errors.txt", "a") as myfile:
+        myfile.write(txt + '\n')
+
 # Load dictionary of admin areas from 2010 csv file
-admin_areas = {}
 def load_admin_areas():
-    global admin_areas
+    result = {
+        'municipality': {},
+        'district': {},
+        'subdistrict': {}
+    }
     with open('composicao_dos_arquivos_da_Base_de_Faces_de_Logradouros_do_CD2010.csv', newline='') as csvfile:
         csv_reader = csv.DictReader(csvfile)
         for row in csv_reader:
-            # Use file id ("Arquivo") as key
-            admin_areas[row['Arquivo']] = {
-                'subdistrict': row['Nome Subdistrito'], 
+            result['municipality'][row['Município']] = {
+                'municipality': row['Nome Município'],
+                'state': row['UF']
+            }
+
+            result['district'][row['Município'] + row['Distrito']] = {
                 'district': row['Nome Distrito'],
                 'municipality': row['Nome Município'],
                 'state': row['UF']
             }
 
-# Load faces from shapefile
-def load_faces(filename):
-    coords = {}
+            result['subdistrict'][row['Arquivo']] = {
+                'subdistrict': row['Nome Subdistrito'],
+                'district': row['Nome Distrito'],
+                'municipality': row['Nome Município'],
+                'state': row['UF']
+            }
 
-    with fiona.open(filename) as shp:
-        for r in shp:
-            if r['geometry'] and r['properties']['CD_FACE']:
-                cd_geo = r['properties']['CD_SETOR'] + \
-                    r['properties']['CD_QUADRA'] + \
-                    r['properties']['CD_FACE']
-                try:
-                    c = shape(r['geometry']).interpolate(0.5, normalized=True)
-                    coords[cd_geo] = c
-                except Exception as err:
-                    print('Invalid geometry: ' + cd_geo, err, file=sys.stderr)
+    return result
 
-    return coords
+# Helper function to convert DMS coordinates to decimal
 
-# For a given shapefile and its id, parse addresses files
-def parse_area(facesid, faces_shp, address_zips):
-    faces = load_faces(faces_shp);
 
-    # Expand addresses files
-    for address_zip in address_zips[0:1]:
-        with zipfile.ZipFile(address_zip, 'r') as zip_ref:
-            zip_ref.extractall(ADDRESSES_PATH)
-
-    for address_txt in glob.glob(ADDRESSES_PATH + '/*.TXT'):        
-        parse_txt(faces, address_txt)
-
-    # Clean temporary directory
-    files = glob.glob(ADDRESSES_PATH + '/*')
-    for f in files:
-        os.remove(f)        
-
-def parse_txt(faces, txt_file):
-    global admin_areas
-    count=0
-
-    print('Parsing file: ' + txt_file)
-    file_basename=os.path.basename(txt_file)
-    file_id=os.path.splitext(file_basename)[0]
-    area=admin_areas[file_id]
-    with open(txt_file, 'r', errors='replace') as lines:
-        for line in lines:
-            parse_line(faces, line, area)
-            count += 1    
-    
-    print('Addresses found: ' + str(count))
-
-def parse_line(faces, line, area):
-    a = {}
-    for (field, start, length) in txt_schema:
-        a[field] = line[start-1:start-1+length].strip()
-
-    # Common fixes
-    a['census sector'] = a['census sector'].replace(' ', '0')
-    a['postal code'] = a['postal code'].zfill(8)
-    if a['number'] == '0':
-        a['number'] = ''
-
-    # Get face id
-    cd_geo = a['census sector'] + a['block'] + a['face']
-
-    # Get lng/lat, if available
-    if a['lon']:
-        try:
-            a['lon'] = dms_to_decimal(a['lon'])
-            a['lat'] = dms_to_decimal(a['lat'])
-        except Exception as err:
-            print('Warning:', err, file=sys.stderr)
-            a['lon'] = ''
-            a['lat'] = ''
-    # Get lng/lat from faces
-    elif cd_geo in faces:
-        a['lon'] = str(faces[cd_geo].x)
-        a['lat'] = str(faces[cd_geo].y)
-
-    # Apply area to record
-    a.update(area)    
-    
-    # Write record
-    csv_writer.writerow(a)
-
-# Helper function to convert DMS coordinates to decimal 
 def dms_to_decimal(c):
     [d, m, s, q] = c.split()
     return (float(d) + float(m)/60 + float(s)/3600) * (-1 if q in ['O', 'S'] else 1)
 
+# Get dictionary of faces from a state zipfile
+
+
+def load_faces_midpoints(state_code, state_zip):
+    midpoints = {}
+    missing_shapefiles=set()
+
+    # Expand zip
+    with zipfile.ZipFile(state_zip, 'r') as zip_ref:
+        zip_ref.extractall(SHP_PATH)
+
+    # Get midpoints from 2019 shapefile and add to dictionary
+    shapefiles_2019 = glob.glob(SHP_PATH + '/*_faces_de_logradouros_2019.shp')
+    for faces_2019_shapefile in shapefiles_2019:
+        print('Loading ' + faces_2019_shapefile + '...')
+        try:
+            with fiona.open(faces_2019_shapefile) as shp:
+                for r in shp:
+                    if r['geometry'] and r['properties']['CD_FACE']:
+                        cd_geo = r['properties']['CD_SETOR'] + \
+                            r['properties']['CD_QUADRA'] + \
+                            r['properties']['CD_FACE']
+                        try:
+                            c = shape(r['geometry']).interpolate(
+                                0.5, normalized=True)
+                            midpoints[cd_geo] = [str(c.x), str(c.y)]
+                        except Exception as err:
+                            print('Invalid geometry: ' +
+                                cd_geo, err, file=sys.stderr)
+        except:
+            missing_shapefiles.add(faces_2019_shapefile)
+
+
+    # Get state id from first key
+    state_id = next(iter(midpoints))[0:2]
+
+    # Get midpoints from 2010 shapefile and add to dictionary
+    zipfiles_2010 = glob.glob(
+        FACES_2010_SOURCE + '/**/'+state_id + '*.zip', recursive=True)
+    for faces_2010_zipfile in zipfiles_2010:
+        with zipfile.ZipFile(faces_2010_zipfile, 'r') as zip_ref:
+            zip_ref.extractall(SHP_PATH)
+
+        # Get sector id
+        file_basename = os.path.basename(faces_2010_zipfile)
+        sector_id = os.path.splitext(file_basename)[0]
+
+        # Get midpoints for sector file
+        faces_2010_shapefile = SHP_PATH+'/' + sector_id + '_face.shp'
+        print('Loading ' + faces_2010_shapefile + '...')
+        try:
+            with fiona.open(faces_2010_shapefile) as shp:
+                for r in shp:
+                    if r['geometry'] and r['properties']['CD_GEO']:
+                        cd_geo = r['properties']['CD_GEO']
+                        try:
+                            c = shape(r['geometry']).interpolate(
+                                0.5, normalized=True)
+                            midpoints[cd_geo] = [str(c.x), str(c.y)]
+                        except Exception as err:
+                            print('Invalid geometry: ' +
+                                cd_geo, err, file=sys.stderr)
+        except:
+            missing_shapefiles.add(faces_2010_shapefile)
+
+    if (len(missing_shapefiles) > 0):
+        with open(RESULTS_PATH + '/'+state_id+'-missing_shapefiles.csv', 'w') as missing_shapefiles_file:
+            missing_shapefiles_file.writelines(
+                "%s\n" % i for i in sorted(missing_shapefiles))            
+
+    return [state_id, midpoints]
+
+
+def parse_addresses(state_id, midpoints, admin_areas):
+    file_stats = {
+        'total': 0,
+        'has_own_coords': 0,
+        'has_face_coords': 0,
+        'missing_coords': 0,
+        'missing_areas': 0
+    }
+    missing_faces = set()
+    missing_areas = set()
+
+    # Get first result from glob, each state contains only one zip
+    [address_zipfile] = glob.glob(
+        ADDRESSES_SOURCE + '/**/'+state_id+'.zip', recursive=True)
+
+    # Expand zipfile
+    print('Expanding ' + address_zipfile + '...')
+    try:
+        with zipfile.ZipFile(address_zipfile, 'r') as zip_ref:
+            zip_ref.extractall(ADDRESSES_PATH)
+    except:
+        log_error("Error expanding "+ address_zipfile)
+        return file_stats
+
+    # Parse textfile
+    address_textfile = ADDRESSES_PATH + '/' + state_id + '.txt'
+    print('Parsing ' + address_textfile + '...')
+    with open(address_textfile, 'r', errors='replace') as lines:
+
+        with open(RESULTS_PATH + '/' + state_id + '.csv', 'w', newline='') as csvfile:
+            csv_writer = csv.DictWriter(
+                csvfile, csv_headers, extrasaction='ignore')
+            csv_writer.writeheader()
+
+            for line in lines:
+                file_stats['total'] += 1
+                addr = {}
+                for (field, start, length) in txt_schema:
+                    addr[field] = line[start-1:start-1+length].strip()
+
+                # Common fixes
+                addr['census sector'] = addr['census sector'].replace(' ', '0')
+                addr['postal code'] = addr['postal code'].zfill(8)
+                if addr['number'] == '0':
+                    addr['number'] = ''
+
+                # Get face id
+                cd_geo = addr['census sector'] + addr['block'] + addr['face']
+
+                # Get lng/lat, if available
+                if addr['lon']:
+                    try:
+                        addr['lon'] = dms_to_decimal(addr['lon'])
+                        addr['lat'] = dms_to_decimal(addr['lat'])
+                        file_stats['has_own_coords'] += 1
+                    except Exception as err:
+                        print('Warning:', err, file=sys.stderr)
+                        addr['lon'] = ''
+                        addr['lat'] = ''
+                # Get lng/lat from faces
+                elif cd_geo in midpoints:
+                    addr['lon'] = midpoints[cd_geo][0]
+                    addr['lat'] = midpoints[cd_geo][1]
+                    file_stats['has_face_coords'] += 1
+                else:
+                    addr['lon'] = ''
+                    file_stats['missing_coords'] += 1
+                    missing_faces.add(cd_geo)
+
+                # If coords available
+                if addr['lon'] != '':
+                    # Apply area to record
+                    subdistrict_id = addr['census sector'][0:11]
+                    if (subdistrict_id) in admin_areas['subdistrict']:
+                        addr.update(admin_areas['subdistrict'][subdistrict_id])
+                    else:
+                        # Fallback to district
+                        district_id = addr['census sector'][0:9]
+                        if (district_id) in admin_areas['district']:
+                            addr.update(admin_areas['district'][district_id])
+                        else:
+                            # Fallback to municipality
+                            file_stats['missing_municipality'] += 1
+                            municipality_id = addr['census sector'][0:7]
+                            if (municipality_id) in admin_areas['municipality']:
+                                addr.update(admin_areas['municipality'][municipality_id])
+
+                    # Check if state is present to confirm area was found
+                    if 'state' in addr:
+                        csv_writer.writerow(addr)
+                    else:
+                        file_stats['missing_areas'] += 1
+                        missing_areas.add(addr['census sector'])
+                    
+
+    with open(RESULTS_PATH + '/'+state_id+'-missing_faces.csv', 'w') as missing_faces_file:
+        missing_faces_file.writelines(
+            "%s\n" % i for i in sorted(missing_faces))
+
+    with open(RESULTS_PATH + '/'+state_id+'-missing_areas.csv', 'w') as missing_areas_file:
+        missing_areas_file.writelines(
+            "%s\n" % i for i in sorted(missing_areas))
+
+    return file_stats
+
+
 # Entry point
-load_admin_areas()
-global csv_writer
+admin_areas = load_admin_areas()
+stats = {}
+
 states_zip = glob.glob(
-    FACES_SOURCE + '/**/*_faces_de_logradouros_2019.zip', recursive=True)
-for state_zip in states_zip[24:25]:
-    state_id=os.path.basename(state_zip)[0:2]
+    FACES_2019_SOURCE + '/**/*_faces_de_logradouros_2019.zip', recursive=True)
+for state_zip in states_zip:
 
     # Clear tmp directory
-    tmp_files=glob.glob('/tmp/**/*', recursive=True)
+    tmp_files = glob.glob('/tmp/**/*', recursive=True)
     for tmp_file in tmp_files:
         if os.path.isfile(tmp_file):
             os.remove(tmp_file)
 
-    print('Parsing state: ' + state_id)
+    state_code = os.path.basename(state_zip)[0:2]
+    print('Parsing state: ' + state_code)
 
-    # Create CSV write stream
-    with open(RESULTS_PATH + '/' + state_id + '.csv', 'w', newline='') as csvfile:
-        csv_writer = csv.DictWriter(csvfile, csv_headers, extrasaction='ignore')
-        csv_writer.writeheader()
+    [state_id, faces_midpoints] = load_faces_midpoints(state_code, state_zip)
 
-        # Expand state shapefiles
-        with zipfile.ZipFile(state_zip, 'r') as zip_ref:
-            zip_ref.extractall(SHP_PATH)
+    stats[state_id] = parse_addresses(state_id, faces_midpoints, admin_areas)
+    stats[state_id]['state_code'] = state_code
 
-        # Parse state shapefiles
-        state_faces_shp = glob.glob(SHP_PATH + '/*_faces_de_logradouros_2019.shp')
-        for faces_shp in state_faces_shp:
-            faces_id = os.path.basename(faces_shp)[0:7]
+# Write stats
+with open(RESULTS_PATH + '/stats.csv', 'w', newline='') as stats_csvfile:
+    headers = ['state_id', 'state_code', 'total',
+               'missing_coords', 'has_own_coords', 'has_face_coords', 'missing_areas']
+    csv_writer = csv.DictWriter(stats_csvfile, headers, extrasaction='ignore')
+    csv_writer.writeheader()
 
-            # Get addresses in shapefile area
-            address_zips = glob.glob(
-                ADDRESSES_SOURCE + '/**/' + faces_id + '*.zip', recursive=True)
-
-            # Parse address file
-            parse_area(faces_id, faces_shp, address_zips);
-
-        # Clean temporary directory
-        files = glob.glob(SHP_PATH + '/*')
-        for f in files:
-            os.remove(f)
+    for item_id, v in stats.items():
+        v.update({'state_id': item_id})
+        csv_writer.writerow(v)
