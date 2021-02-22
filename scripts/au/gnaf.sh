da4ff8f3417c1539ecb9570ff051cdce0e890200
Andrew Harvey <andrew@alantgeo.com.au>
2021-02-22 19:09:04 +1100

    update au/countrywide for FEB21 release

--- scripts/au/gnaf.sh
+++ scripts/au/gnaf.sh
@@ -17,10 +17,10 @@ sudo -u postgres psql -c 'CREATE DATABASE gnafdb OWNER gnafun TABLESPACE gnafts'
 sudo -u postgres psql -c 'CREATE EXTENSION postgis'
 
 # fetch data/resources, cached from:
-## https://data.gov.au/data/dataset/bdcf5b09-89bc-47ec-9281-6b8e9ee147aa
-## https://data.gov.au/data/dataset/19432f89-dc3a-4ef3-b943-5326ef1dbecc
-curl --retry 10 --location 'https://data.gov.au/data/dataset/bdcf5b09-89bc-47ec-9281-6b8e9ee147aa/resource/53c24b8e-4f55-4eed-a189-2fc0dcca6381/download/nov20_adminbounds_esrishapefileordbffile.zip' -o $TMP/gnaf-admin.zip
-curl --retry 10 --location 'https://data.gov.au/data/dataset/19432f89-dc3a-4ef3-b943-5326ef1dbecc/resource/4b084096-65e4-4c8e-abbe-5e54ff85f42f/download/nov20_gnaf_pipeseparatedvalue.zip' -o $TMP/gnaf.zip
+## https://data.gov.au/data/dataset/geoscape-administrative-boundaries
+## https://data.gov.au/data/dataset/geocoded-national-address-file-g-naf
+curl --retry 10 --location 'https://data.gov.au/data/dataset/bdcf5b09-89bc-47ec-9281-6b8e9ee147aa/resource/53c24b8e-4f55-4eed-a189-2fc0dcca6381/download/feb21_adminbounds_esrishapefileordbffile.zip' -o $TMP/gnaf-admin.zip
+curl --retry 10 --location 'https://data.gov.au/data/dataset/19432f89-dc3a-4ef3-b943-5326ef1dbecc/resource/4b084096-65e4-4c8e-abbe-5e54ff85f42f/download/feb21_gnaf_pipeseparatedvalue.zip' -o $TMP/gnaf.zip
 parallel "unzip -d $TMP/{} $TMP/{}.zip" ::: gnaf gnaf-admin
 rm -f $TMP/gnaf.zip $TMP/gnaf-admin.zip
 
@@ -89,7 +89,7 @@ chmod a+w $TMP/au.csv
 echo "COPY openaddresses TO '$TMP/au.csv' DELIMITER ',' CSV HEADER;" | psql -t -q postgres://gnafun:gnafpw@localhost/gnafdb
 
 mkdir /work/cache
-zip -j /work/cache/au-nov2020.zip $TMP/au.csv
+zip -j /work/cache/au-feb2021.zip $TMP/au.csv
 
 # clean up temporary files
 /etc/init.d/postgresql stop
