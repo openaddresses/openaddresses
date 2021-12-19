26324a609e12f525e733062831a73239b5162ea1
ingalls <nick@ingalls.ca>
2021-12-19 08:03:43 -0600

    Add Ref Parser

--- test/lib.js
+++ test/lib.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 const path = require('path');
+const $RefParser = require('json-schema-ref-parser');
 
-const GEOJSON = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../schema/geojson.json')));
+const GEOJSON = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../schema/util/geojson.json')));
 const SCHEMA_V2 = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../schema/source_schema_v2.json')));
 
-// Only returns actively supported versions
+class OASchema {
+    static async compile() {
+        await $RefParser.dereference(path.resolve(path.resolve(__dirname, '../schema/'), 'source_schema_v2.json'));
+    }
+}
+
 module.exports = {
     geojson: GEOJSON,
     schema: {
