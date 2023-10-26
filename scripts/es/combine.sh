e261bbcde3e357b36c7da0a9a659bef43737d66d
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-10-26 17:22:38 +0200

    How to run

--- scripts/es/combine.sh
+++ scripts/es/combine.sh
@@ -1,7 +1,7 @@
 # write header
 rm -f es.csv
 touch es.csv
-ls build | grep "es-" | head -n 1 | xargs -I {} head -n 1 build/{} > tee es.csv
+ls build | grep "es-" | head -n 1 | xargs -I {} head -n 1 build/{} > es.csv
 
 # combine CSVs, stripping headers
 for a in $(find build | grep csv)
