16a34308cdc7080de551478b41dd153f6b8098f0
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-10-30 17:14:04 +0100

    Update to make es script (Spain) working with current python and current data (#6968)
    * Use standard tabs in Makefile
    
    * Use dependent targets in Makefile
    
    * Swith scripts from python 2 to 3
    
    * How to run
    
    * Strip street name
    
    * Replace parallel with xargs, python script cannot make parallel write into the ouput files

--- scripts/es/gml_to_csv.py
+++ scripts/es/gml_to_csv.py
@@ -1,6 +1,6 @@
 import subprocess, sys, os, tempfile, shutil, re, zipfile
 import xml.sax
-import unicodecsv as csv
+import csv
 
 lookup = {}
 
@@ -41,7 +41,7 @@ class CSVBuilder(xml.sax.ContentHandler):
             lookup_key = self.strip_hash.sub('', attrs['xlink:href'])
 
             if self.lookup['thoroughfare'].get(lookup_key) is not None:
-                self.object['street'] = self.lookup['thoroughfare'].get(lookup_key)
+                self.object['street'] = self.lookup['thoroughfare'].get(lookup_key).strip()
             elif self.lookup['admin'].get(lookup_key) is not None:
                 self.object['admin'] = self.lookup['admin'].get(lookup_key)
             elif self.lookup['postal'].get(lookup_key) is not None:
@@ -118,7 +118,7 @@ class LookupBuilder(xml.sax.ContentHandler):
             self.lookup[self.lookup['_type']][self.lookup['_next']] += content
 
 def process_zipfile(in_filename, out_dir):
-    print 'converting %s, placing output into %s' % (in_filename, out_dir)
+    print('converting %s, placing output into %s' % (in_filename, out_dir))
 
     temp_dir = tempfile.mkdtemp()
 
@@ -131,14 +131,14 @@ def process_zipfile(in_filename, out_dir):
 
     # build thoroughfare/postcode lookup
     lookup = LookupBuilder()
-    with open(filename, 'r') as gml:
+    with open(filename, 'r', encoding='ISO-8859-1') as gml:
         parser = xml.sax.make_parser()
         parser.setContentHandler(lookup)
         parser.parse(gml)
 
     # generate complete CSV
     csv_builder = CSVBuilder(out_dir, lookup)
-    with open(filename, 'r') as gml:
+    with open(filename, 'r', encoding='ISO-8859-1') as gml:
         parser = xml.sax.make_parser()
         parser.setContentHandler(csv_builder)
         parser.parse(gml)
